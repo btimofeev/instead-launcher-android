84c6a6bf478a01915940b7b529494feebc8deb13
Boris Timofeev <mashin87@gmail.com>
2021-03-07 16:30:32 +0300

    navigation to settings fragment inside launcher activity

--- app/src/main/kotlin/org/emunix/insteadlauncher/ui/settings/SettingsFragment.kt
+++ app/src/main/kotlin/org/emunix/insteadlauncher/ui/settings/SettingsFragment.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020 Boris Timofeev <btimofeev@emunix.org>
+ * Copyright (c) 2018-2021 Boris Timofeev <btimofeev@emunix.org>
  * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
  */
 
@@ -7,10 +7,13 @@ package org.emunix.insteadlauncher.ui.settings
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import androidx.preference.EditTextPreference
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
+import com.google.android.material.appbar.MaterialToolbar
 import org.emunix.insteadlauncher.InsteadLauncher
 import org.emunix.insteadlauncher.R
 import org.emunix.insteadlauncher.helpers.ThemeHelper
@@ -18,6 +21,15 @@ import org.emunix.insteadlauncher.services.UpdateRepositoryWork
 
 
 class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val toolbar: MaterialToolbar = view.findViewById(R.id.toolbar) as MaterialToolbar
+        toolbar.setNavigationIcon(R.drawable.ic_back_24dp)
+        toolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
@@ -27,25 +39,25 @@ class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedP
                                            key: String) {
         when (key) {
             "pref_repository" -> {
-                val repo : EditTextPreference? = findPreference("pref_repository")
+                val repo: EditTextPreference? = findPreference("pref_repository")
                 if (repo != null && repo.text.isBlank()) {
                     repo.text = InsteadLauncher.DEFAULT_REPOSITORY
                 }
             }
             "pref_sandbox" -> {
-                val sandbox : EditTextPreference? = findPreference("pref_sandbox")
+                val sandbox: EditTextPreference? = findPreference("pref_sandbox")
                 if (sandbox != null && sandbox.text.isBlank()) {
                     sandbox.text = InsteadLauncher.SANDBOX
                 }
             }
             "app_theme" -> {
-                val theme : ListPreference? = findPreference("app_theme")
+                val theme: ListPreference? = findPreference("app_theme")
                 if (theme != null) {
                     ThemeHelper.applyTheme(theme.value)
                 }
             }
             "pref_update_repo_background" -> {
-                val pref : SwitchPreference? = findPreference("pref_update_repo_background")
+                val pref: SwitchPreference? = findPreference("pref_update_repo_background")
                 if (pref != null && pref.isChecked) {
                     UpdateRepositoryWork.start(requireContext())
                 } else {
