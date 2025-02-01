24147b70e05735c85c1e5a023f01aec5c394e5fe
Boris Timofeev <mashin87@gmail.com>
2025-02-01 10:09:16 +0300

    rewrite about screen to compose

--- app/src/main/kotlin/org/emunix/insteadlauncher/presentation/about/AboutFragment.kt
+++ app/src/main/kotlin/org/emunix/insteadlauncher/presentation/about/AboutFragment.kt
@@ -6,37 +6,46 @@
 package org.emunix.insteadlauncher.presentation.about
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
+import android.view.ViewGroup
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import org.emunix.insteadlauncher.BuildConfig
 import org.emunix.insteadlauncher.R
-import org.emunix.insteadlauncher.databinding.FragmentAboutBinding
 import org.emunix.insteadlauncher.domain.repository.AppVersionRepository
+import org.emunix.insteadlauncher.presentation.theme.InsteadLauncherTheme
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class AboutFragment : Fragment(R.layout.fragment_about) {
+class AboutFragment : Fragment() {
 
     @Inject
     lateinit var appVersion: AppVersionRepository
 
-    private val binding by viewBinding(FragmentAboutBinding::bind)
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
-
-        binding.toolbar.setNavigationIcon(R.drawable.ic_back_24dp)
-        binding.toolbar.setNavigationOnClickListener {
-            findNavController().popBackStack()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
+        val composeView = view.findViewById<ComposeView>(R.id.compose_view)
+        composeView.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                InsteadLauncherTheme {
+                    AboutScreen(
+                        insteadVersion = BuildConfig.INSTEAD_VERSION,
+                        insteadLauncherVersion = appVersion.versionName,
+                        onBackClick = { findNavController().popBackStack() }
+                    )
+                }
+            }
         }
-
-        binding.aboutInstead.text = getString(R.string.about_activity_about_instead, BuildConfig.INSTEAD_VERSION)
-        binding.aboutInsteadLauncher.text =
-            getString(R.string.about_activity_about_instead_launcher, appVersion.versionName)
+        return view
     }
 }
---
+++ app/src/main/kotlin/org/emunix/insteadlauncher/presentation/about/AboutScreen.kt
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2025 Boris Timofeev <btimofeev@emunix.org>
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
+
+package org.emunix.insteadlauncher.presentation.about
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.emunix.insteadlauncher.R
+import org.emunix.insteadlauncher.presentation.theme.InsteadLauncherTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AboutScreen(
+    insteadVersion: String,
+    insteadLauncherVersion: String,
+    onBackClick: () -> Unit
+) {
+    Scaffold(
+        modifier = Modifier.safeDrawingPadding(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(stringResource(R.string.about_activity_title))
+                },
+                navigationIcon = {
+                    IconButton(onClick = { onBackClick.invoke() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null
+                        )
+                    }
+                },
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.boy_and_cat),
+                contentDescription = null,
+                modifier = Modifier.size(300.dp),
+                contentScale = ContentScale.Fit,
+            )
+            TextBlock(stringResource(R.string.about_activity_about_instead, insteadVersion))
+            TextBlock(stringResource(R.string.about_activity_about_instead_launcher, insteadLauncherVersion))
+            TextBlock(stringResource(R.string.about_activity_thanks))
+            TextBlock(stringResource(R.string.about_activity_to_game_authors))
+            TextBlock(stringResource(R.string.about_activity_discuss))
+            Spacer(Modifier.height(16.dp))
+        }
+    }
+}
+
+@Composable
+private fun TextBlock(
+    text: String,
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodyLarge,
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+    )
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 400,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
+@Preview(showBackground = true, widthDp = 400)
+@Composable
+private fun AboutScreenPreview() {
+    InsteadLauncherTheme {
+        AboutScreen(
+            insteadVersion = "3.5",
+            insteadLauncherVersion = "0.9.1",
+            onBackClick = {},
+        )
+    }
+}
