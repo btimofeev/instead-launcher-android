e05d4d2e07ce3dc3535165c1f69be8ec9c16e7a5
Boris Timofeev <mashin87@gmail.com>
2025-02-01 11:11:53 +0300

    rewrite unpack resources screen to compose

--- app/src/main/kotlin/org/emunix/insteadlauncher/presentation/unpackresources/UnpackResourcesFragment.kt
+++ app/src/main/kotlin/org/emunix/insteadlauncher/presentation/unpackresources/UnpackResourcesFragment.kt
@@ -1,68 +1,69 @@
 /*
- * Copyright (c) 2021-2023 Boris Timofeev <btimofeev@emunix.org>
+ * Copyright (c) 2021-2023, 2025 Boris Timofeev <btimofeev@emunix.org>
  * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
  */
 
 package org.emunix.insteadlauncher.presentation.unpackresources
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import androidx.core.view.isVisible
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
-import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import org.emunix.insteadlauncher.R
-import org.emunix.insteadlauncher.databinding.FragmentUnpackResourcesBinding
 import org.emunix.insteadlauncher.presentation.models.UnpackResourcesScreenState.ERROR
 import org.emunix.insteadlauncher.presentation.models.UnpackResourcesScreenState.SUCCESS
 import org.emunix.insteadlauncher.presentation.models.UnpackResourcesScreenState.UNPACKING
+import org.emunix.insteadlauncher.presentation.theme.InsteadLauncherTheme
 
 @AndroidEntryPoint
-class UnpackResourcesFragment : Fragment(R.layout.fragment_unpack_resources) {
-
-    private val binding by viewBinding(FragmentUnpackResourcesBinding::bind)
+class UnpackResourcesFragment : Fragment() {
 
     private val viewModel: UnpackResourcesViewModel by viewModels()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setupViews()
-        setupObservers()
-    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_compose, container, false)
+        val composeView = view.findViewById<ComposeView>(R.id.compose_view)
+        composeView.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                val state by viewModel.screenState.collectAsState()
 
-    private fun setupViews() {
-        binding.unpackResourcesTryAgainButton.setOnClickListener { viewModel.tryAgainIsClicked() }
-    }
-
-    private fun setupObservers() {
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.screenState.collect { state ->
-                    when (state) {
-                        SUCCESS -> navigateToInstalledGamesScreen()
-                        UNPACKING -> showError(false)
-                        ERROR -> showError(true)
+                InsteadLauncherTheme {
+                    Scaffold(modifier = Modifier.safeDrawingPadding()) { innerPadding ->
+                        Box(modifier = Modifier.padding(innerPadding)) {
+                            when (state) {
+                                SUCCESS -> navigateToInstalledGamesScreen()
+                                UNPACKING -> UnpackResourcesLoadingScreen()
+                                ERROR -> UnpackResourcesErrorScreen(
+                                    onTryAgainClick = viewModel::tryAgainIsClicked
+                                )
+                            }
+                        }
                     }
                 }
             }
         }
+        return view
     }
 
     private fun navigateToInstalledGamesScreen() {
         findNavController().navigate(R.id.action_unpackResourcesFragment_to_installedGamesFragment)
-    }
-
-    private fun showError(isErrorVisible: Boolean) {
-        with(binding) {
-            unpackResourcesProgressBar.isVisible = !isErrorVisible
-            errorTextView.isVisible = isErrorVisible
-            unpackResourcesTryAgainButton.isVisible = isErrorVisible
-        }
     }
 }
