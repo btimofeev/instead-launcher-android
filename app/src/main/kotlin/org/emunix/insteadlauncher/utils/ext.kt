f9fa5e8e7e6bba6ed71f86f0cabafa155d7558cb
Boris Timofeev <btimofeev@emunix.org>
2025-12-13 10:15:19 +0300

    remove unused libs

--- app/src/main/kotlin/org/emunix/insteadlauncher/utils/ext.kt
+++ app/src/main/kotlin/org/emunix/insteadlauncher/utils/ext.kt
@@ -7,16 +7,7 @@ package org.emunix.insteadlauncher.utils
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.InsetDrawable
 import android.net.Uri
-import android.text.TextUtils
-import android.view.View
-import android.widget.ImageView
-import androidx.annotation.DimenRes
-import androidx.recyclerview.widget.DividerItemDecoration
-import coil.load
-import coil.size.Precision.EXACT
 import org.apache.commons.io.FileUtils
 import org.apache.commons.io.IOUtils
 import org.emunix.insteadlauncher.R
@@ -27,33 +18,8 @@ import java.io.ByteArrayInputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.io.InputStream
-import java.util.*
 import java.util.zip.ZipException
 import java.util.zip.ZipInputStream
-
-fun ImageView.loadUrl(url: String, highQuality: Boolean = false) {
-    load(url.ifEmpty { R.drawable.sleeping_cat }) {
-        placeholder(R.drawable.walking_cat)
-        error(R.drawable.sleeping_cat)
-        if (highQuality) {
-            precision(EXACT)
-        }
-    }
-}
-
-fun DividerItemDecoration.insetDivider(context: Context, @DimenRes startOffsetDimension: Int): Drawable {
-    val a = context.obtainStyledAttributes(intArrayOf(android.R.attr.listDivider))
-    val divider = a.getDrawable(0)
-    a.recycle()
-    val inset = context.resources.getDimensionPixelSize(startOffsetDimension)
-    val isLeftToRight = TextUtils.getLayoutDirectionFromLocale(Locale.getDefault()) == View.LAYOUT_DIRECTION_LTR
-
-    return if (isLeftToRight) {
-        InsetDrawable(divider, inset, 0, 0, 0)
-    } else {
-        InsetDrawable(divider, 0, 0, inset, 0)
-    }
-}
 
 private const val BUFFER_SIZE = 102400
 
