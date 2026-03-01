9f8bdc4954748def5b46b75499e93d856a20db84
Boris Timofeev <mashin87@gmail.com>
2026-03-01 11:21:46 +0300

    clickable links

--- app/src/main/kotlin/org/emunix/insteadlauncher/presentation/about/AboutScreen.kt
+++ app/src/main/kotlin/org/emunix/insteadlauncher/presentation/about/AboutScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -35,6 +36,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.emunix.insteadlauncher.R
+import org.emunix.insteadlauncher.presentation.compose.parseLinks
 import org.emunix.insteadlauncher.presentation.theme.InsteadLauncherTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -88,8 +90,10 @@ fun AboutScreen(
 private fun TextBlock(
     text: String,
 ) {
+    val linkColor = MaterialTheme.colorScheme.primary
+    val annotatedString = remember(text) { parseLinks(text, linkColor) }
     Text(
-        text = text,
+        text = annotatedString,
         style = MaterialTheme.typography.bodyLarge,
         modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
     )
