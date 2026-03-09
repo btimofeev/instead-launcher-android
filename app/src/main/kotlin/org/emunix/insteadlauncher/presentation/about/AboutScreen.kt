13b5face549538334367b89ab8023134728f7f43
Boris Timofeev <btimofeev@emunix.org>
2026-03-09 16:10:42 +0300

    tablets support (part 1)
    about, settings and gameinfo screens

--- app/src/main/kotlin/org/emunix/insteadlauncher/presentation/about/AboutScreen.kt
+++ app/src/main/kotlin/org/emunix/insteadlauncher/presentation/about/AboutScreen.kt
@@ -8,6 +8,7 @@ package org.emunix.insteadlauncher.presentation.about
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -36,6 +38,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import org.emunix.insteadlauncher.BuildConfig
@@ -82,24 +85,36 @@ fun AboutScreenContent(
             )
         }
     ) { innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding)
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
                 .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+            contentAlignment = Alignment.TopCenter,
         ) {
-            Image(
-                painter = painterResource(R.drawable.boy_and_cat),
-                contentDescription = null,
-                modifier = Modifier.size(300.dp),
-                contentScale = ContentScale.Fit,
-            )
-            TextBlock(stringResource(R.string.about_activity_about_instead, insteadVersion))
-            TextBlock(stringResource(R.string.about_activity_about_instead_launcher, insteadLauncherVersion))
-            TextBlock(stringResource(R.string.about_activity_thanks))
-            TextBlock(stringResource(R.string.about_activity_to_game_authors))
-            TextBlock(stringResource(R.string.about_activity_discuss))
-            Spacer(Modifier.height(16.dp))
+            Column(
+                modifier = Modifier.widthIn(max = 500.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.boy_and_cat),
+                    contentDescription = null,
+                    modifier = Modifier.size(300.dp),
+                    contentScale = ContentScale.Fit,
+                )
+                TextBlock(stringResource(R.string.about_activity_about_instead, insteadVersion))
+                TextBlock(
+                    stringResource(
+                        R.string.about_activity_about_instead_launcher,
+                        insteadLauncherVersion
+                    )
+                )
+                TextBlock(stringResource(R.string.about_activity_thanks))
+                TextBlock(stringResource(R.string.about_activity_to_game_authors))
+                TextBlock(stringResource(R.string.about_activity_discuss))
+                Spacer(Modifier.height(16.dp))
+            }
         }
     }
 }
@@ -124,6 +139,7 @@ private fun TextBlock(
     name = "Dark"
 )
 @Preview(showBackground = true, widthDp = 400)
+@PreviewScreenSizes
 @Composable
 private fun AboutScreenContentPreview() {
     InsteadLauncherTheme {
