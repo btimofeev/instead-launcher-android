8659712776dca028cc573e23180fbcd59b9c81c1
Boris Timofeev <btimofeev@emunix.org>
2026-03-09 09:01:30 +0300

    change default app theme

--- app/src/main/kotlin/org/emunix/insteadlauncher/presentation/theme/Theme.kt
+++ app/src/main/kotlin/org/emunix/insteadlauncher/presentation/theme/Theme.kt
@@ -1,6 +1,5 @@
 package org.emunix.insteadlauncher.presentation.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,13 +8,8 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
